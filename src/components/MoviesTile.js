21bf4ce1a7132ae4784ba1638bc663b1aed85411
Jacob Marthaller <marthaller.jr@gmail.com>
2021-03-25 08:46:50 -0700

    delete cruft from MoviesTile.js

--- src/components/MoviesTile.js
+++ src/components/MoviesTile.js
@@ -10,13 +10,9 @@ function MoviesTile({ id, title, genre, runtime, tagline, release_year, image, s
 
     return (
             <Link style={{textDecoration: "none"}} onClick={handleResetSearch} to={`/movies/${id}`}>
-        <div style={{backgroundImage: `url(${image})`}} className="movie-tile">
-            <h1 style={{visibility: "hidden"}}>{title}</h1>
-            {/* <img  src={image} alt={id}></img> */}
-            {/* <h4>{release_year}</h4> */}
-            {/* <h5>{tagline}</h5> */}
-                {/* Review {title} */}
-        </div>
+                <div style={{backgroundImage: `url(${image})`}} className="movie-tile">
+                    <h1 style={{visibility: "hidden"}}>{title}</h1>
+                </div>
             </Link>
     )
 }
