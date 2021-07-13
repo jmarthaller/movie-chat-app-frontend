974a9595ffe62938e1b7c6619253f98e137f9493
Jacob Marthaller <marthaller.jr@gmail.com>
2021-07-13 12:09:24 -0700

    Reformat UserReviews with Prettier

--- src/components/UserReviews.js
+++ src/components/UserReviews.js
@@ -1,120 +1,156 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
+function UserReviews({
+  id,
+  author,
+  authorImage,
+  movieTitle,
+  movieImage,
+  content,
+  onUpdateReview,
+  onDeleteReview,
+  personalRating,
+  canEditAccount,
+}) {
+  const [canEditReview, setCanEditReview] = useState(false);
+  const [canDeleteReview, setCanDeleteReview] = useState(false);
+  const [updatedContent, setUpdatedContent] = useState("");
+  const [updatedRating, setUpdatedRating] = useState(null);
 
-function UserReviews({ id, author, authorImage, movieTitle, movieImage, content, onUpdateReview, onDeleteReview, personalRating, canEditAccount}) {
-    const [canEditReview, setCanEditReview] = useState(false)
-    const [canDeleteReview, setCanDeleteReview] = useState(false)
-    const [updatedContent, setUpdatedContent] = useState('')
-    const [updatedRating, setUpdatedRating] = useState(null)
+  function toggleEditReview() {
+    setCanEditReview(!canEditReview);
+  }
 
+  function toggleDeleteReview() {
+    setCanDeleteReview(!canDeleteReview);
+  }
 
-    function toggleEditReview() {
-        setCanEditReview(!canEditReview)
-    }
+  function handleContentChange(event) {
+    setUpdatedContent(event.target.value);
+  }
 
+  function handleUpdateRating(event) {
+    setUpdatedRating(event.target.value);
+  }
 
-    function toggleDeleteReview() {
-        setCanDeleteReview(!canDeleteReview)
-    }
+  async function handleUpdateReview(e) {
+    e.preventDefault();
 
+    const formData = {
+      content: updatedContent,
+      personal_rating: updatedRating,
+    };
 
-    function handleContentChange(event) {
-        setUpdatedContent(event.target.value);
-    }
-
-
-    function handleUpdateRating(event) {
-        setUpdatedRating(event.target.value)
-    }
-
-
-    async function handleUpdateReview(e) {
-        e.preventDefault()
-        
-        const formData = {
-            content: updatedContent,
-            personal_rating: updatedRating
-        }
-
-        const response = await fetch(`${process.env.REACT_APP_RAILS_URL}/reviews/${id}`, {
-        method: "PATCH", 
+    const response = await fetch(
+      `${process.env.REACT_APP_RAILS_URL}/reviews/${id}`,
+      {
+        method: "PATCH",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({"content": formData.content, personal_rating: parseInt(formData.personal_rating)}),
-        })
-        const jsonify = await response.json()
-        onUpdateReview(jsonify, formData)
-        setUpdatedContent("")
-        setCanEditReview()
-    }
+        body: JSON.stringify({
+          content: formData.content,
+          personal_rating: parseInt(formData.personal_rating),
+        }),
+      }
+    );
+    const jsonify = await response.json();
+    onUpdateReview(jsonify, formData);
+    setUpdatedContent("");
+    setCanEditReview();
+  }
 
+  function handleDeleteReview() {
+    fetch(`${process.env.REACT_APP_RAILS_URL}/reviews/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    onDeleteReview(id);
+  }
 
-    function handleDeleteReview() {
-        fetch(`${process.env.REACT_APP_RAILS_URL}/reviews/${id}`, {
-        method: "DELETE", 
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        onDeleteReview(id)
-    }
-
-    return (
-        <div className="user-reviews">
-            <hr className="profile-hr" />
-            <img src={movieImage} alt={movieTitle}></img>
-            <div className="movie-review-details">
-            <h2>{movieTitle}</h2>
-            <h4>{content}</h4>
-            <h4>Your rating: {personalRating}</h4>
-            { canEditReview || canEditAccount ? <button className="edit-review-button" style={{zIndex: "-1"}} onClick={toggleEditReview}>EDIT REVIEW</button> : <button className="edit-review-button" onClick={toggleEditReview}>EDIT REVIEW</button>}
-            { canEditReview ?
-          
-            <div className="review-modal-content">
-                <div className="review-modal-header">
-                    <span className="review-close" onClick={toggleEditReview}>&times;</span>
-                    <h2>Edit Your Review for {movieTitle}</h2>
-                </div>    
-                <div className="review-modal-body">
-                    <form onSubmit={handleUpdateReview}>
-                        <textarea 
-                            className="edit-review-text" 
-                            type="text" 
-                            name="content" 
-                            value={updatedContent} 
-                            onChange={handleContentChange}>
-                        </textarea>
-                        <br></br>
-                        <select className="edit-star-rating" onChange={handleUpdateRating}>
-                            <option value=''>Update Rating</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                        </select>
-                        <br></br>
-                        <input type="submit" value="Submit" className="submit-edit-review-btn" />
-                    </form>
-                    <hr className="profile-hr"></hr>
-                    <button className="delete-review-button" onClick={toggleDeleteReview}>DELETE REVIEW</button>
-                    { canDeleteReview 
-                        ?
-                            <div className="confirm-delete-review-container">
-                                <h2>Are you SURE you want to delete this review?</h2>
-                                <button className="delete-review-button" onClick={handleDeleteReview}>Yes, Delete</button>
-                            </div>
-                        :   null 
-                    }
-                </div>  
+  return (
+    <div className="user-reviews">
+      <hr className="profile-hr" />
+      <img src={movieImage} alt={movieTitle}></img>
+      <div className="movie-review-details">
+        <h2>{movieTitle}</h2>
+        <h4>{content}</h4>
+        <h4>Your rating: {personalRating}</h4>
+        {canEditReview || canEditAccount ? (
+          <button
+            className="edit-review-button"
+            style={{ zIndex: "-1" }}
+            onClick={toggleEditReview}
+          >
+            EDIT REVIEW
+          </button>
+        ) : (
+          <button className="edit-review-button" onClick={toggleEditReview}>
+            EDIT REVIEW
+          </button>
+        )}
+        {canEditReview ? (
+          <div className="review-modal-content">
+            <div className="review-modal-header">
+              <span className="review-close" onClick={toggleEditReview}>
+                &times;
+              </span>
+              <h2>Edit Your Review for {movieTitle}</h2>
             </div>
-            : null
-            }
+            <div className="review-modal-body">
+              <form onSubmit={handleUpdateReview}>
+                <textarea
+                  className="edit-review-text"
+                  type="text"
+                  name="content"
+                  value={updatedContent}
+                  onChange={handleContentChange}
+                ></textarea>
+                <br></br>
+                <select
+                  className="edit-star-rating"
+                  onChange={handleUpdateRating}
+                >
+                  <option value="">Update Rating</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+                <br></br>
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="submit-edit-review-btn"
+                />
+              </form>
+              <hr className="profile-hr"></hr>
+              <button
+                className="delete-review-button"
+                onClick={toggleDeleteReview}
+              >
+                DELETE REVIEW
+              </button>
+              {canDeleteReview ? (
+                <div className="confirm-delete-review-container">
+                  <h2>Are you SURE you want to delete this review?</h2>
+                  <button
+                    className="delete-review-button"
+                    onClick={handleDeleteReview}
+                  >
+                    Yes, Delete
+                  </button>
+                </div>
+              ) : null}
             </div>
-        </div>
-    )
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
 }
-
 
 export default UserReviews;
